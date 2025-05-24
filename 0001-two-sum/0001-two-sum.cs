071828aa64a11bfe571a7a6bc74ea87d527f3ed1
Md Rahad <rahad3100@gmail.com>
2025-05-24 23:10:42 +0600

    Time: 1 ms (98.4%), Space: 49.2 MB (16.09%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var dic = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dic.ContainsKey(nums[i]))
+                return new int[] { dic[nums[i]], i };
+            else
+                dic[target - nums[i]] = i;
+        }
+
+        return new int[] { };
+    }
+}
