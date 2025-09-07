9150f8d5126313dbe3edf6a4ec3ba8a96e7a33f1
Md Rahad <rahad3100@gmail.com>
2025-09-07 23:47:53 +0600

    Time: 4 ms (60.49%), Space: 47.2 MB (34.62%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public string IntToRoman(int num) {
+        // Arrays for Roman symbols and their values
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        string result = "";
+
+        for (int i = 0; i < values.Length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                result += symbols[i];
+            }
+        }
+
+        return result;
+    }
+}
